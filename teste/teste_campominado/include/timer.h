2933b5da061e616920baf09bcde87774ab15ec77
josevvito <165079983+josevvito@users.noreply.github.com>
2025-05-13 13:40:08 -0400

    Add files via upload

---
+++ teste/teste_campominado/include/timer.h
@@ -0,0 +1,11 @@
+
+
+void timerInit(int valueMilliSec);
+
+void timerDestroy();
+
+void timerUpdateTimer(int valueMilliSec);
+
+int timerTimeOver();
+
+void timerPrint();
---
+++ teste/teste_campominado/src/main.c
@@ -0,0 +1,12 @@
+#include <string.h>
+#include "screen.h"
+#include "keyboard.h"
+#include "timer.h"
+
+void executarJogo();
+
+int main() 
+{
+    executarJogo();
+    return 0;
+}
